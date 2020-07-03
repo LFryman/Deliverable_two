8a19be7a783710cd6e7205c95d543165037aee2e
lauren <lauren.a.fryman@gmail.com>
2020-07-02 23:11:13 -0400

    First commit for Deliverable two

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+
+namespace Deliverable2
+{
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            //gets user input and store in var string input. 
+            Console.Write("What is your message? ");
+            string input = Console.ReadLine();
+
+            string message = "";
+            int checksum = 0;
+            
+            // accessing individual characters and prints 
+            for (int i = 0; i < input.Length; i++)
+            {
+                int unicodeval = char.ToUpper(input[i]);
+                int numPlacement = unicodeval - 64;
+
+                //finding checksum through our table 
+                checksum = checksum + unicodeval;
+
+                message = message + numPlacement.ToString() + "-";
+
+            }
+
+            Console.WriteLine("Your encoded message is " + message);
+            Console.WriteLine("Message checksum is " + checksum);
+            
+
+        }
+    }
+}
